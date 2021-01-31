e66a8f9748f799bbff8416deef0c566e223d882d
Petr Zimin <petrzl@bk.ru>
2021-01-31 18:46:27 +0300

    added some logic for video playback

--- app/src/main/java/ru/petrzimin/basicsvideo/MainActivity.java
+++ app/src/main/java/ru/petrzimin/basicsvideo/MainActivity.java
@@ -3,12 +3,23 @@ package ru.petrzimin.basicsvideo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+    MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.somevideo);
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(mediaController);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
     }
 }
